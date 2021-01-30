0cd43438d41df9207424a342bed63d833b5389d0
byznrdmrr <byznrrdmr@gmail.com>
2021-01-30 22:38:03 +0300

    2018 Güz Dönemi

---
+++ BSM205 - WEB PROGRAMLAMA/PROJE ÖDEVİ/benimmutfağım.com/MyEvernote.BusinessLayer/MyEvernote.BusinessLayer/Abstract/ManagerBase.cs
@@ -0,0 +1,56 @@
+﻿using MyEvernote.Core.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using MyEvernote.DataAccessLayer.EntityFramework;
+
+namespace MyEvernote.BusinessLayer.Abstract
+{
+    public abstract class ManagerBase<T> : IDataAccess<T> where T : class
+    {
+        private Repository<T> repo = new Repository<T>();
+
+        public virtual int Delete(T obj)
+        {
+            return repo.Delete(obj);
+        }
+
+        public virtual T Find(Expression<Func<T, bool>> where)
+        {
+            return repo.Find(where);
+        }
+
+        public virtual int Insert(T obj)
+        {
+            return repo.Insert(obj);
+        }
+
+        public virtual List<T> List()
+        {
+            return repo.List();
+        }
+
+        public virtual List<T> List(Expression<Func<T, bool>> where)
+        {
+            return repo.List(where);
+        }
+
+        public virtual IQueryable<T> ListQueryable()
+        {
+            return repo.ListQueryable();
+        }
+
+        public virtual int Save()
+        {
+            return repo.Save();
+        }
+
+        public virtual int Update(T obj)
+        {
+            return repo.Update(obj);
+        }
+    }
+}

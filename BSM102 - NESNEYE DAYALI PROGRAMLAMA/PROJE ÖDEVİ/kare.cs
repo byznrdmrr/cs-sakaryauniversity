353b86c6dd8203dbd1e631240f3f706972c73bc9
byznrdmrr <byznrrdmr@gmail.com>
2021-01-30 19:19:33 +0300

    2017 Bahar Dönemi

---
+++ BSM102 - NESNEYE DAYALI PROGRAMLAMA/PROJE ÖDEVİ/kare.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication21
+{
+    class kare:sekil
+    {
+        public float width;
+        Pen pen;
+       
+
+        public kare(Color renk)
+        {
+            width = 0;
+            pen = new Pen(renk, 3);
+            
+        }
+
+        public override void BitisAta(float bx, float by)
+        {
+            width = bx - X;
+           
+        }
+
+        public override void ciz(Graphics cizimAraci)
+        {
+            cizimAraci.DrawRectangle(pen, X, Y, width, width);
+            
+        }
+    }
+    
+}

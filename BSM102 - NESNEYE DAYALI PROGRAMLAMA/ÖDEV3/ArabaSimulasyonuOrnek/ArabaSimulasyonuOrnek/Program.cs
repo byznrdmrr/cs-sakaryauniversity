d5bd177719e5961d17a4820d871bed04dd7bc4a7
byznrdmrr <byznrrdmr@gmail.com>
2021-01-30 20:48:11 +0300

    2017 Bahar Dönemi

---
+++ BSM102 - NESNEYE DAYALI PROGRAMLAMA/ÖDEV3/ArabaSimulasyonuOrnek/ArabaSimulasyonuOrnek/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ArabaSimulasyonuOrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Bu program sınıfları test etmek için yazılmıştır ve
+             * ödeve dâhil değildir.
+             * Ödevde sadece sınıflar ve arayüzler istenmektedir. */
+            
+            var kontakAnahtari = new KontakAnahtari();
+            var motor = new BenzinliMotor();
+
+            var elektronikBeyin = new ElektronikBeyin(kontakAnahtari, motor);
+
+            // Kontak anahtarı açılıyor
+            // Bu durum elektronik beyne otomatik iletilir,
+            // beyin de otomatik olarak motoru çalıştırır.
+            kontakAnahtari.Ac();
+
+            Console.ReadKey();
+        }
+    }
+}
